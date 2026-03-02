f12fba6dee1a4b64fd77e9ad91bc8ba4ff1b7094
Idriss Foka <idrissfoka748@gmail.com>
2026-03-02 19:01:00 +0100

    select all available jobs at the current moment and apply to each manually currently waiting for more jobs to be added to the website so that i can test the SaaS

--- accept.py
+++ accept.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 def accept_cookies(driver, cookie_element):
         try:
-            cookie_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, cookie_element)))
+            cookie_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, cookie_element)))
             cookie_button.click()
             print("Cookie clicked")
             time.sleep(2)  # Wait for popup to close
